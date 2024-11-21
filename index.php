8f855c96047055aa75c69bacc58072722ee8adfd
Markus Helfenstein <markus.helfenstein@gmail.com>
2024-11-21 14:56:07 +0100

    sanitize and validate input

--- index.php
+++ index.php
@@ -14,9 +14,11 @@
         <input type="radio" name="payment_method" id="payment_method_2" value="American Express"><label for="payment_method_2">American Express</label><br>
         <input type="checkbox" name="foods[]" id="food_0" value="Hamburger"><label for="food_0">Hamburger</label><br>
         <input type="checkbox" name="foods[]" id="food_1" value="Pizza"><label for="food_1">Pizza</label><br>
-        <label for="username">username:</label><br>
+        <label for="email">email: </label><br>
+        <input type="text" name="email"><br>
+        <label for="username">username: </label><br>
         <input type="text" name="username"><br>
-        <label for="password">password:</label><br>
+        <label for="password">password: </label><br>
         <input type="password" name="password"><br>
         <input type="submit" name="login" value="log in"><br>
     </form>
@@ -24,10 +26,23 @@
 
 </html>
 <?php
+function filter_sanitize_post($key) {
+    return filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
 foreach ($_POST as $key => $value) {
+    $value = filter_sanitize_post($key);
     if (is_array($value)) {
         $concatenatedArrayString = implode(",", $value);
         echo "{$key} => [{$concatenatedArrayString}] <br>";
+    } elseif(0 == strcmp($key, "email")) {
+        // does not to_lower/to_upper
+        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+        if(empty($email)) {
+            echo "Entered value is not a valid email address!<br>";
+        } else {
+            echo "{$key} => <a href='mailto:{$email}'>{$email}</a> <br>";
+        }
     } else {
         echo "{$key} => {$value} <br>";
     }
