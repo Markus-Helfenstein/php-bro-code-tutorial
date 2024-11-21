aa3f864c23550841c91dd0168deac54ffdea7683
Markus Helfenstein <markus.helfenstein@gmail.com>
2024-11-21 11:04:55 +0100

    string interpolation and arithmetic operations

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php 
     echo "Hello World!";
+
+    $prize = 4.99;
+    echo "Your pizza is \${$prize}<br>";
+
+    $sideLength = 10;
+    $area = $sideLength ** 2;
+    echo "A square slice with a side length of {$sideLength}cm has an area of {$area}cm<sup>2</sup><br>";
 ?>
