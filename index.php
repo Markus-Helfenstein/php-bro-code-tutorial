fba95ac494a4c253f2c72c7582dde0891fce6dba
Markus Helfenstein <markus.helfenstein@gmail.com>
2024-11-21 14:00:46 +0100

    radio and checkboxes

--- index.php
+++ index.php
@@ -8,17 +8,28 @@
 </head>
 
 <body>
-    
+    <form action="index.php" method="post">
+        <input type="radio" name="payment_method" id="payment_method_0" value="Visa"><label for="payment_method_0">Visa</label><br>
+        <input type="radio" name="payment_method" id="payment_method_1" value=" MasterCard"><label for="payment_method_1">MasterCard</label><br>
+        <input type="radio" name="payment_method" id="payment_method_2" value="American Express"><label for="payment_method_2">American Express</label><br>
+        <input type="checkbox" name="foods[]" id="food_0" value="Hamburger"><label for="food_0">Hamburger</label><br>
+        <input type="checkbox" name="foods[]" id="food_1" value="Pizza"><label for="food_1">Pizza</label><br>
+        <label for="username">username:</label><br>
+        <input type="text" name="username"><br>
+        <label for="password">password:</label><br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="login" value="log in"><br>
+    </form>
 </body>
 
 </html>
 <?php
-    $foods = ["apple", "orange", "banana", "coconut"];
-
-    array_push($foods, "pineapple", "kiwi");
-    $foods = array_reverse($foods);
-
-    foreach($foods as $food){
-        echo "{$food}<br>";
+foreach ($_POST as $key => $value) {
+    if (is_array($value)) {
+        $concatenatedArrayString = implode(",", $value);
+        echo "{$key} => [{$concatenatedArrayString}] <br>";
+    } else {
+        echo "{$key} => {$value} <br>";
     }
+}
 ?>
